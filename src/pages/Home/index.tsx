f9b7040e7e1172bccdeecc3b33eccc5f64e5283a
thiagodnt <thiagodnt@hotmail.com>
2026-02-10 21:40:15 -0300

    feat: implementar service api products e listagem dos produtos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react';
 import { BsCartPlus } from 'react-icons/bs';
+import { api } from '../../services/api';
+
+interface ProductProps {
+	id: number;
+	title: string;
+	description: string;
+	price: number;
+	cover: string;
+}
 
 const Home = () => {
+	const [products, setProducts] = useState<ProductProps[]>([]);
+
+	useEffect(() => {
+		async function getProducts() {
+			const response = await api.get('/products');
+			setProducts(response.data);
+		}
+
+		getProducts();
+	}, []);
+
 	return (
 		<div>
 			<main className="w-full max-w-7xl px-4 mx-auto">
@@ -8,21 +29,29 @@ const Home = () => {
 					Produtos em Alta
 				</h1>
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
-					<section className="w-full">
-						<img
-							className="w-full rounded-lg max-h-70 mb-2"
-							src="https://images.tcdn.com.br/img/img_prod/1324137/notebook_gamer_rog_strix_g17_g713pv_ws94_amd_ryzen_9_7845hx_tela_wqhd_17_3_16gb_de_ram_1tb_ssd_gefor_731_2_436052eaf2198bde0074f52459c192c3.jpg"
-							alt="Imagem do Produto"
-						/>
-						<p className="font-medium mt-1 mb-2">Notebook Gamer</p>
+					{products &&
+						products.map((product) => (
+							<section key={product.id} className="w-full">
+								<img
+									className="w-full rounded-lg max-h-70 mb-2"
+									src={product.cover}
+									alt={product.title}
+								/>
+								<p className="font-medium mt-1 mb-2">{product.title}</p>
 
-						<div className="flex gap-3 items-center">
-							<strong className="text-zinc-700/90">R$ 5000.00</strong>
-							<button className="bg-zinc-800 p-1 rounded transition-transform hover:scale-105 cursor-pointer">
-								<BsCartPlus size={20} color="#FFF" />
-							</button>
-						</div>
-					</section>
+								<div className="flex gap-3 items-center">
+									<strong className="text-zinc-700/90">
+										{product.price.toLocaleString('pt-BR', {
+											style: 'currency',
+											currency: 'BRL',
+										})}
+									</strong>
+									<button className="bg-zinc-800 p-1 rounded transition-transform hover:scale-105 cursor-pointer">
+										<BsCartPlus size={20} color="#FFF" />
+									</button>
+								</div>
+							</section>
+						))}
 				</div>
 			</main>
 		</div>
