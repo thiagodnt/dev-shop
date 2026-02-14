21ee7095bde420b3b00774effd784d20b03eb79a
thiagodnt <thiagodnt@hotmail.com>
2026-02-14 14:34:30 -0300

    feat: listar produtos do carrinho

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,4 +1,9 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
+
 const Cart = () => {
+	const { cart } = useContext(CartContext);
+
 	return (
 		<div>
 			<main className="w-full max-w-7xl px-4 mx-auto">
@@ -6,28 +11,43 @@ const Cart = () => {
 					Meu carrinho
 				</h1>
 
-				<section className="flex items-center justify-between border-b-2 border-gray-300">
-					<img
-						className="w-28"
-						src="https://images.tcdn.com.br/img/img_prod/1324137/notebook_gamer_rog_strix_g17_g713pv_ws94_amd_ryzen_9_7845hx_tela_wqhd_17_3_16gb_de_ram_1tb_ssd_gefor_731_2_436052eaf2198bde0074f52459c192c3.jpg"
-						alt="Imagem do Produto"
-					/>
+				{cart.map((item) => (
+					<section
+						key={item.id}
+						className="flex items-center justify-between border-b-2 border-gray-300"
+					>
+						<img className="w-28" src={item.cover} alt={item.title} />
 
-					<strong>Preço: R$ 1000.00</strong>
+						<strong>
+							Preço:{' '}
+							{item.price.toLocaleString('pt-BR', {
+								style: 'currency',
+								currency: 'BRL',
+							})}
+						</strong>
 
-					<div className="flex items-center justify-center gap-3">
-						<button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center transition-transform hover:scale-105 cursor-pointer">
-							-
-						</button>
-						1
-						<button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center transition-transform hover:scale-105 cursor-pointer">
-							+
-						</button>
-					</div>
+						<div className="flex items-center justify-center gap-3">
+							<button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center transition-transform hover:scale-105 cursor-pointer">
+								-
+							</button>
+							{item.amount}
+							<button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center transition-transform hover:scale-105 cursor-pointer">
+								+
+							</button>
+						</div>
 
-					<strong className="float-right">SubTotal: R$ 1000.00</strong>
-				</section>
-				<p className="font-bold mt-4">Total: R$ 1000.00</p>
+						<strong className="float-right">
+							SubTotal:{' '}
+							{item.total.toLocaleString('pt-BR', {
+								style: 'currency',
+								currency: 'BRL',
+							})}
+						</strong>
+					</section>
+				))}
+				{cart.length > 0 && (
+					<p className="font-bold mt-4">Total: R$ 1000.00</p>
+				)}
 			</main>
 		</div>
 	);
