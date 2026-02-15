8409a4d95c622cdbfd68cd9ef77971655bd2966c
thiagodnt <thiagodnt@hotmail.com>
2026-02-15 14:27:25 -0300

    chore: add strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { router } from './App.tsx';
@@ -7,10 +7,10 @@ import CartProvider from './contexts/CartContext.tsx';
 import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
-	<>
+	<StrictMode>
 		<CartProvider>
 			<Toaster position="top-center" reverseOrder={false} />
 			<RouterProvider router={router} />
 		</CartProvider>
-	</>,
+	</StrictMode>,
 );
